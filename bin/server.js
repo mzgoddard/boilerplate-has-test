31576336483b52b909d58e07e632b256503f06f0
Michael "Z" Goddard <mzgoddard@gmail.com>
2012-09-23 13:00:32 -0400

    improve server shortcut script for its new place

--- bin/server.js
+++ bin/server.js
@@ -1,19 +1,35 @@
+var path = require( 'path' ),
+    spawn = require( 'child_process' ).spawn;
+
+process.chdir( path.normalize( __dirname + '/../lib/' ) );
+
 if ( process.argv.length === 2 || process.argv[ 2 ] !== 'node' ) {
 
   var r = require( 'requirejs' );
   r.config( {
     nodeRequire: require,
     mainConfigFile: 'config',
+    baseUrl: '../lib/',
     name: 'config' } );
 
   r( 'config' );
 
 } else {
 
-  // either set node path for vender libs or create fake node_modules
-  process.env.NODE_PATH = '../vendor/';
+  try { 
+    require( 'has' );
 
-  require( './hastests' );
-  require( './main' );
+    require( '../lib/hastests' );
+    require( '../lib/main' );
+  } catch ( e ) {
+    var vendorPath = path.normalize( __dirname + '/../vendor/' );
+
+    process.env.NODE_PATH = vendorPath;
+    var child = spawn( 'node', [ 'server.js', 'node' ], {
+      cwd: __dirname
+    } );
+    child.stdout.pipe( process.stdout );
+    child.stderr.pipe( process.stderr );
+  }
 
 }
